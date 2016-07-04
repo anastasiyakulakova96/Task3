385bd0c1de4c214564da743d1bc4f60e01da05dd
anastasiyakulakova96 <anastasiya_kulakova@epam.com>
2016-07-04 21:13:34 +0300

    add test class

---
+++ Disigne/UnitTest/UnitTestClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestClass
+    {
+        [TestMethod]
+        public void TestAnyMethod()
+        {
+        }
+    }
+}
